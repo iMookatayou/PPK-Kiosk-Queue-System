c121fe4e63f5768f3e0cd6a9d438c146dea42620
iMookatayou <jetsribumrungtechin@gmail.com>
2025-07-22 09:59:44 +0700

    Queue V5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
 // app/layout.tsx
 import './globals.css'
-import { autoResetOnStartup } from '@/lib/queue-server-admin' // เลี่ยนชื่อและ path
 
 export const metadata = {
   title: 'ตู้กดหมายเลขรับบริการ',
   description: 'ค้นหาข้อมูลผู้ป่วยด้วย HN',
 }
 
-export default async function RootLayout({  
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  await autoResetOnStartup() // เรียกใช้ตัวที่มีจริง
   return (
     <html lang="th">
       <body>{children}</body>
